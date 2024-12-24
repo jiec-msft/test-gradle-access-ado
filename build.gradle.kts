dea4225c5f9d6b43372767fd7f3d42d49e93eb95
Mason Chen <jiec@microsoft.com>
2024-12-24 20:52:50 +0800

    Configure the ADO access

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,23 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    maven {
+        url = uri("https://pkgs.dev.azure.com/jiec-temp-org/Tooling/_packaging/mason-test-feed-in-personal-org/maven/v1")
+        credentials  {
+            username = ""
+            password = project.findProperty("ado.artifacts.jiec-temp-org.mason-test-feed-in-personal-org.token") as String
+        }
+    }
 }
 
 dependencies {
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
